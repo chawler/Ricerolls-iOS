7440394b7fd286976476f25b3ebbb956c173ca90
阿东 <konaod@gmail.com>
2016-06-15 07:14:03 +0800

    Initial Commit

---
+++ Ricerolls-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ricerolls-iOS
+//
+//  Created by 郑先生 on 16/6/15.
+//  Copyright © 2016年 郑先生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
